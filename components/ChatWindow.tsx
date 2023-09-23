33d30d75b79a3e11e299670fb69985870b19147c
scicluna <88948700+scicluna@users.noreply.github.com>
2023-09-23 15:56:29 -0400

    adding some ui

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -2,6 +2,14 @@
 import { Message, useChat } from 'ai/react';
 import { useEffect, useRef, useState } from 'react';
 import { PlayerBlock } from '@/utils/player';
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 
 export default function Chatwindow() {
     const [player, setPlayer] = useState<PlayerBlock>(new PlayerBlock())
@@ -81,7 +89,7 @@ export default function Chatwindow() {
     }
 
     return (
-        <div className="mx-auto w-full flex flex-col">
+        <div className="mx-auto w-full flex flex-col relative">
             <div className='flex gap-2 w-full h-full p-2'>
                 <div className='h-[80dvh] w-2/3 flex flex-col gap-4 overflow-auto bg-gray-400 bg-opacity-30' ref={chatContainer}>
                     {messages.map((m, i) => (
@@ -106,6 +114,49 @@ export default function Chatwindow() {
                 />
                 <button className='bg-purple-600 p-2 rounded-lg hover:bg-purple-500' type="submit" disabled={isLoading}>Send</button>
             </form>
+
+            <Sheet>
+                <SheetTrigger className='fixed bottom-0 left-2 p-2 h-[10dvh] text-2xl font-extrabold  underline hover:text-gray-600'>Character Sheet</SheetTrigger>
+                <SheetContent side={'bottom'} className='h-[20dvh]'>
+                    <SheetHeader>
+                        <SheetDescription>
+                            <div className='flex gap-8'>
+                                <div className='flex flex-col text-justify'>
+                                    <SheetTitle>Hero</SheetTitle>
+                                    <p>str: {player.str} ({Math.floor((player.str - 10) / 2)})</p>
+                                    <p>dex: {player.dex} ({Math.floor((player.dex - 10) / 2)})</p>
+                                    <p>con: {player.con} ({Math.floor((player.con - 10) / 2)})</p>
+                                    <p>int: {player.int} ({Math.floor((player.int - 10) / 2)})</p>
+                                    <p>wis: {player.wis} ({Math.floor((player.wis - 10) / 2)})</p>
+                                    <p>cha: {player.cha} ({Math.floor((player.cha - 10) / 2)})</p>
+                                </div>
+                                <div className='flex flex-col text-justify'>
+                                    <SheetTitle>Attributes</SheetTitle>
+                                    <p>hp: {player.hp}/{player.hp}</p>
+                                    <p>ac: {player.ac}</p>
+                                    <p>atk: {player.atk}</p>
+                                    <p>dmg: {player.dmg}</p>
+                                    <p>wpn: {player.wpn}</p>
+                                    <p>armor: {player.armor}</p>
+                                </div>
+                                <div className='flex flex-col text-justify w-1/2 flex-wrap'>
+                                    <SheetTitle>Inventory</SheetTitle>
+                                    {player.inventory.map(item => (
+                                        <p key={item.item} className='w-fit'> {item.quantity}x {item.item} {item.type} {item.stat ?? null}</p>
+                                    ))}
+                                </div>
+                                <div className='flex flex-col text-justify overflow-auto'>
+                                    <SheetTitle>Journal</SheetTitle>
+                                    {player.journal.map(entry => (
+                                        <p key={entry.number}>{entry.entry}</p>
+                                    ))}
+                                </div>
+                            </div>
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
+
         </div>
     );
 }
